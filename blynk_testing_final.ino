facf14d373986113c8268e2cc88ae5120ed9fd76
luciferTheSinner <sanchitawasthi2525@gmail.com>
2023-05-09 03:30:09 +0530

    first commit

---
+++ blynk_testing_final.ino
@@ -0,0 +1,72 @@
+/*************************************************************
+  Blynk is a platform with iOS and Android apps to control
+  ESP32, Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build mobile and web interfaces for any
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: https://www.blynk.io
+    Sketch generator:           https://examples.blynk.cc
+    Blynk community:            https://community.blynk.cc
+    Follow us:                  https://www.fb.com/blynkapp
+                                https://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+ *************************************************************
+  Blynk.Edgent implements:
+  - Blynk.Inject - Dynamic WiFi credentials provisioning
+  - Blynk.Air    - Over The Air firmware updates
+  - Device state indication using a physical LED
+  - Credentials reset using a physical Button
+ *************************************************************/
+
+/* Fill in information from your Blynk Template here */
+/* Read more: https://bit.ly/BlynkInject */
+//#define BLYNK_TEMPLATE_ID           "TMPxxxxxx"
+//#define BLYNK_TEMPLATE_NAME         "Device"
+#define BLYNK_TEMPLATE_ID "TMPL3sLBjH9iW"
+#define BLYNK_TEMPLATE_NAME "NodeMCU"
+
+#define BLYNK_FIRMWARE_VERSION        "0.1.0"
+
+#define BLYNK_PRINT Serial
+//#define BLYNK_DEBUG
+
+#define APP_DEBUG
+
+// Uncomment your board, or configure a custom board in Settings.h
+//#define USE_SPARKFUN_BLYNK_BOARD
+//#define USE_NODE_MCU_BOARD
+//#define USE_WITTY_CLOUD_BOARD
+//#define USE_WEMOS_D1_MINI
+
+#include "BlynkEdgent.h"
+
+double speed;
+BLYNK_WRITE(V0)
+{
+  if(param.asInt()==1)
+  digitalWrite(2,HIGH);
+  else
+  digitalWrite(2,LOW);
+}
+
+BLYNK_CONNECTED()
+{
+  Blynk.syncVirtual(V0);
+  Blynk.virtualWrite(V1,speed);
+}
+
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(100);
+    pinMode(2,OUTPUT);
+
+  BlynkEdgent.begin();
+}
+
+void loop() {
+  speed=1000.00;
+  BlynkEdgent.run();
+}
